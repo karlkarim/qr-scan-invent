f4411189d31e7d2cbd27b4b3886117002be78bd4
Rain <rkullamaa@gmail.com>
2020-03-14 15:56:27 +0100

    added return item logic

--- src/components/Sections/ItemScan/index.js
+++ src/components/Sections/ItemScan/index.js
@@ -1,11 +1,17 @@
-import React, { useState, setGlobal } from 'reactn';
+import React, { useState, useGlobal, setGlobal } from 'reactn';
 import QrReader from 'react-qr-reader'
 import Modal from '../../Modals/modal';
 import firebase from "../../../firebase";
 import './index.css';
-const ItemScan = () => {
-  const [scanned, setScanned] = useState('')
+import Button, { STYLES } from '../../Buttons/button';
 
+const ItemScan = () => {
+  // const [dialogState, setdialogState] = useState('')
+  const [ dialogState, setDialogState ] = useGlobal('dialogState');
+  const [ loggedInUserData ] = useGlobal('loggedInUserData');
+  const [ msg, setMsg ] = useGlobal('notificationMsg');
+  const [ item, setItem ] = useState('')
+  const [ returnItem, setReturnItem ] = useState(false)
   const handleScan = data => {
     if (data) {
       // eslint-disable-next-line
@@ -14,25 +20,72 @@ const ItemScan = () => {
 
       docRef.get().then(function(doc) {
         if (doc.exists && doc.data().status === 'OUT') {
-          setGlobal({
-            dialogState: true,
-            // returnItem: true
-          })
+          setDialogState(true);
+          setReturnItem(true);
         } else {
-          setGlobal({
-            modalOpen: true,
-            returnItem: false
-          })
+          setDialogState(true);
+          setReturnItem(false);
           console.log("No such document!");
         }
       }).catch(function(error) {
         console.log("Error getting document:", error);
       });
-        setScanned(item)
+        setItem(item)
     }
   }
   const handleError = err => {
     console.error(err)
+  }
+  const takeObject = (item) => (
+    <div className='return-message'>
+      <h4>You are about to take {item} from storage ?</h4>
+      <p>If you want to continue click Take button.</p>
+      <Button buttonStyle={STYLES[3]} onClick={() => console.log('TODO function for taking')} children='Take'/>
+      <Button buttonStyle={STYLES[4]} onClick={() => setDialogState(false)} children='Cancel'/>
+    </div>
+  )
+  const returnObject = (item) => {
+    let docRef = firebase.firestore().collection("items").doc(item);
+    let message = `You have returned ${item} successfully !`;
+    const returnAction = async (item)  => {
+      const addLogs = await firebase.firestore().collection('activityLogs').add({
+        action: 'UPDATE',
+        msg: 'Retuned an item',
+        user: `${loggedInUserData[0].firstName} ${loggedInUserData[0].lastName}`,
+        item: item,
+        created_at: new Date()
+    })
+      await docRef.get().then(function(doc) {
+      if (doc.data().status === 'OUT') {
+          firebase.firestore()
+              .collection("items")
+              .doc(`${item}`)
+              .set({returnedDate: new Date(), takenBy: loggedInUserData[0].firstName, status:'IN', location: 'storage'},{merge:true})
+              .then(function() {
+                setDialogState(false);
+                setMsg({show: true, msg:message, variant: 'success'})
+              })
+              .catch(function(error) {
+                  console.error("Error writing document: ", error);
+                  setMsg({show: true, msg:error, variant: 'error'})
+              });
+              return addLogs;
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("Something is fucked!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  })
+}
+    return(
+    <div className='return-message'>
+      <h4>Returning {item} back to storage ?</h4>
+      <p>If yes click the return button.</p>
+      <Button buttonStyle={STYLES[3]} onClick={() => returnAction(item)} children='Return'/>
+      <Button buttonStyle={STYLES[4]} onClick={() => setDialogState(false)} children='Cancel'/>
+    </div>
+    )
   }
     return ( 
         <section>
@@ -53,8 +106,8 @@ const ItemScan = () => {
                 </div>
           </div>
     <Modal
-      header={'lolo'}
-      body={scanned}
+      header={returnItem ? 'Return item' : 'Take Item'}
+      body={returnItem ? returnObject(item) : takeObject(item)}
     />
     </section>
      );
